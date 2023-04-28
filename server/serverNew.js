96964315be5a53c9779d970de6f4a3f64c9412d3
AmoraMas <amoramas1984@gmail.com>
2023-04-28 08:21:06 -0600

    Got Redis working

--- server/serverNew.js
+++ server/serverNew.js
@@ -1,5 +1,9 @@
+/** @format */
+
 // define dependencies
-import express  from "express";
+import express from "express";
+import { createClient } from "redis";
+
 //const cors = require("cors");
 const server = express();
 const port = process.env.PORT || 5172; // Port that Express listens to for requests
@@ -7,16 +11,22 @@ const port = process.env.PORT || 5172; // Port that Express listens to for reque
 server.use(express.json());
 
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 // define structure for accessing database
 //const { Pool } = require('pg');
 import getPool from "./connDB.js";
 const sql = getPool();
+
+// define redis info
+const redisClient = createClient({ socket: { port: 6379, host: "redis" } });
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+await redisClient.connect();
+const redisExpiration = 300; // 300 seconds or 5 minutes
 
 // listen to the port
 server.listen(port, function () {
@@ -29,93 +39,117 @@ server.listen(port, function () {
 
 // test request to verify this file is working
 server.get("/test", (req, res) => {
-    console.log('Responded to test request.');
-      res.json('You have reached the server routing page');
+  console.log("Responded to test request.");
+  res.json("You have reached the server routing page");
+});
+
+server.get("/language", (req, res) => {
+  const result = sql.query(`SELECT * FROM language`, (err, result) => {
+    res.json(result.rows);
   });
-  
-  server.get("/language", (req, res) => {
-    const result = sql.query(`SELECT * FROM language`, (err, result) => {
-      res.json(result.rows);
-    });
+});
+
+server.get("/currency", (req, res) => {
+  const result = sql.query(`SELECT * FROM currency;`, (err, result) => {
+    res.json(result.rows);
   });
-  
-  server.get("/currency", (req, res) => {
-    const result = sql.query(`SELECT * FROM currency;`, (err, result) => {
-      res.json(result.rows);
-    });
+});
+
+server.get("/amenities", (req, res) => {
+  const result = sql.query(`SELECT * FROM amenities`, (err, result) => {
+    res.json(result.rows);
   });
-  
-  server.get("/amenities", (req, res) => {
-    const result = sql.query(`SELECT * FROM amenities`, (err, result) => {
-      res.json(result.rows);
-    });
+});
+
+server.get("/users", (req, res) => {
+  const result = sql.query(`SELECT * FROM users`, (err, result) => {
+    res.json(result.rows);
   });
-  
-  server.get("/users", (req, res) => {
-      const result = sql.query(`SELECT * FROM users`, (err, result) => {
-        res.json(result.rows);
-      });
-    });
-  
-    server.get("/properties", (req, res) => {
-      const result = sql.query(`SELECT * FROM properties`, (err, result) => {
-        res.json(result.rows);
-      });
-    });
-  
-    server.get("/user_properties", (req, res) => {
-      const result = sql.query(`SELECT * FROM user_properties`, (err, result) => {
-        res.json(result.rows);
-      });
-    });
-  
-    server.get("/propertyImages", (req, res) => {
-      const result = sql.query(`SELECT * FROM propertyImages`, (err, result) => {
-        res.json(result.rows);
-      });
-    });
-  
-    server.get("/reviews", (req, res) => {
-      const result = sql.query(`SELECT * FROM reviews`, (err, result) => {
-        res.json(result.rows);
-      });
-    });
-  
-    server.get("/properties/:id/reviews", (req, res, next) => {
-      const { id } = req.params;
-      const result = sql.query(`
+});
+
+server.get("/properties", (req, res) => {
+  const result = sql.query(`SELECT * FROM properties`, (err, result) => {
+    res.json(result.rows);
+  });
+});
+
+server.get("/user_properties", (req, res) => {
+  const result = sql.query(`SELECT * FROM user_properties`, (err, result) => {
+    res.json(result.rows);
+  });
+});
+
+server.get("/propertyImages", (req, res) => {
+  const result = sql.query(`SELECT * FROM propertyImages`, (err, result) => {
+    res.json(result.rows);
+  });
+});
+
+server.get("/reviews", (req, res) => {
+  const result = sql.query(`SELECT * FROM reviews`, (err, result) => {
+    res.json(result.rows);
+  });
+});
+
+server.get("/properties/:id/reviews", async (req, res, next) => {
+  const { id } = req.params;
+  let results = redisClient.get(`reviewsbyid${id}`, async (error, result) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+  results = await results;
+  if (results != null) {
+    res.json(JSON.parse(results));
+  } else {
+    const result = await new Promise((resolve, reject) =>
+      sql.query(
+        `
         SELECT r.*, u.avatar, u.firstname, u.lastname
         FROM reviews r
         JOIN users u ON r.userid = u.userid
         WHERE r.propertyid = ${id}
-      `
-        , (err, result)  => {
-          res.json(result.rows);
-        })
-     });
-  
-    server.post("/reviews", function (req, res, next) {
-      const requiredKeys = ["propertyid", "review", "rating", "userid"];
-      if (
-        requiredKeys.every((key) => req.body.hasOwnProperty(key))
-      ) {
-        const result = sql.query(`INSERT INTO reviews(propertyid, review, rating, userid) VALUES (${req.body.propertyid}, ${req.body.review}, ${req.body.rating}, ${req.body.userid}) RETURNING *;`,
-        (err, result, next) => {
-          if (result)
-            res.status(201);
-            res.json(result[0]);
-          })
-      } else {
-        res.status(400).send("Bad Request");
-        console.log(req.body);
+        `,
+        (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data.rows);
+          }
+        }
+      )
+    );
+    let response = await result;
+    redisClient.setEx(
+      `reviewsbyid${id}`,
+      redisExpiration,
+      JSON.stringify(response)
+    );
+    res.json(response);
+  }
+});
+
+server.post("/reviews", function (req, res, next) {
+  const requiredKeys = ["propertyid", "review", "rating", "userid"];
+  if (requiredKeys.every((key) => req.body.hasOwnProperty(key))) {
+    const result = sql.query(
+      `INSERT INTO reviews(propertyid, review, rating, userid) VALUES (${req.body.propertyid}, ${req.body.review}, ${req.body.rating}, ${req.body.userid}) RETURNING *;`,
+      (err, result, next) => {
+        if (result) res.status(201);
+        res.json(result[0]);
       }
-    });
-  
-    server.use((req, res, next) => {
-      res.contentType("text/plain").status(404).send("Not Found");
-    });
-    
-    server.use((err, req, res, next) => {
-      console.error(err);
-      res.status(500).send("Internal Server Error");
-    });
+    );
+  } else {
+    res.status(400).send("Bad Request");
+    console.log(req.body);
+  }
+});
+
+server.use((req, res, next) => {
+  res.contentType("text/plain").status(404).send("Not Found");
+});
+
+server.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Internal Server Error");
+});
